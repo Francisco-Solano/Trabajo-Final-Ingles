1d7fe837d15ba959816d171129241317aaca5a51
Francisco-Solano <franciscosolanoleon@gmail.com>
2025-02-13 18:05:16 +0100

    correccion DOM

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-const textos = [
-  "Enhance your professional future with our business skills training solutions.",
-  "Master essential office tools and boost your career with our expert courses.",
-  "Customized training for businesses looking to improve their team's productivity.",
-  "Unlock your potential with our business skills courses, designed for success.",
-  "Elevate your business with our office software training and services."
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const textos = [
+    "Enhance your professional future with our business skills training solutions.",
+    "Master essential office tools and boost your career with our expert courses.",
+    "Customized training for businesses looking to improve their team's productivity.",
+    "Unlock your potential with our business skills courses, designed for success.",
+    "Elevate your business with our office software training and services."
+  ];
 
-let index = 0;
-const dynamicText = document.getElementById("dynamic-text");
+  let index = 0;
+  const dynamicText = document.getElementById("dynamic-text");
 
-// Aseguramos que el primer texto sea visible inmediatamente
-dynamicText.classList.add("visible");
+  // Aseguramos que el primer texto sea visible inmediatamente
+  dynamicText.classList.add("visible");
 
-function cambiarTexto() {
-  dynamicText.classList.remove("visible"); // Eliminar la clase 'visible' antes de cambiar el texto
+  function cambiarTexto() {
+    dynamicText.classList.remove("visible"); // Eliminar la clase 'visible' antes de cambiar el texto
 
-  setTimeout(() => {
-      dynamicText.textContent = textos[index];
-      dynamicText.classList.add("visible"); // Añadir la clase 'visible' después de cambiar el texto
-      
-      index = (index + 1) % textos.length;  // Cambiar al siguiente texto en el array
-  }, 300); // Esperar 300ms para la transición de 'opacity' antes de actualizar el texto
-}
+    setTimeout(() => {
+        dynamicText.textContent = textos[index];
+        dynamicText.classList.add("visible"); // Añadir la clase 'visible' después de cambiar el texto
+        
+        index = (index + 1) % textos.length;  // Cambiar al siguiente texto en el array
+    }, 300); // Esperar 300ms para la transición de 'opacity' antes de actualizar el texto
+  }
 
-setInterval(cambiarTexto, 5000); // Cambia cada 5 segundos
+  setInterval(cambiarTexto, 5000); // Cambia cada 5 segundos
+});
+
+
+
+
 
 
 
